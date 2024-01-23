5a2f56904dd4cfff4abf5e6d00fc6126b971e3f4
roman <romasaab@gmail.com>
2024-01-23 20:28:39 +0200

    first commit

---
+++ source/js/_vars.js
@@ -0,0 +1,17 @@
+export default {
+  activeClass: "active",
+  windowEl: window,
+  documentEl: document,
+  htmlEl: document.documentElement,
+  bodyEl: document.body,
+
+  header: document.querySelector('header'),
+  overlay: document.querySelector('[data-overlay]'),
+  mobileMenu: document.querySelector('.mobile-menu'),
+  burger: document.querySelector('.burger'),
+}
+
+
+
+
+
