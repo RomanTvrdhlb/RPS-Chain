daeb7ff96717b37cf85692ef7bf241709d5b0954
roman <romasaab@gmail.com>
2024-01-27 18:09:07 +0200

    add animation coins && flash

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,11 +1,11 @@
 import './components/sliders';
 import './components/countdown';
 import './components/burger';
-import './components/getDinamicHeight';
+// import './components/getDinamicHeight';
 import './components/accordions';
 import './components/replaceEl';
 import './components/aos';
-import './components/stickyHeader';
+// import './components/stickyHeader';
 
 
 
